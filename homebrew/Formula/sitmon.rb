cfc4ab10d6f5fc6e55f771464aa12987969a15d0
Joshua-Wright76 <jwrightbluj@gmail.com>
2026-03-23 11:02:14 -0700

    Update formula with correct SHA256 for v0.1.0

--- homebrew/Formula/sitmon.rb
+++ homebrew/Formula/sitmon.rb
@@ -2,7 +2,7 @@ class Sitmon < Formula
   desc "Rust TUI situation monitor"
   homepage "https://github.com/Joshua-Wright76/sitmontui"
   url "https://github.com/Joshua-Wright76/sitmontui/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "TBD"
+  sha256 "2cde48081c6ec5f45214c14ee11ba921a8d51b2792c750d709cf06cd85703188"
   license "MIT"
   head "https://github.com/Joshua-Wright76/sitmontui.git"
 
